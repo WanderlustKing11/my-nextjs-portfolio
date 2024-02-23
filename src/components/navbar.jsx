4b48e9f3cae86e9a601b44d35a311f9f96eeb14e
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-23 12:56:24 -0500

    Update navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import NavLink from './navLink';
 
 const links = [
   { url: '/', title: 'Home' },
@@ -16,16 +17,16 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
       {/* NAV LINKS */}
       <div className='hidden md:flex gap-4 w-1/3'>
         {links.map(link => (
-          <Link href={link.url} key={link.title}>{link.title}</Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
 
       {/* LOGO */}
-      <div className='md:hidden lg:flex w-1/3'>
+      <div className='md:hidden lg:flex xl:w-1/3 xl:justify-center'>
         <Link href='/' className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
           <span className='text-white mr-1'>Devy</span>
           <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>Doug</span>
