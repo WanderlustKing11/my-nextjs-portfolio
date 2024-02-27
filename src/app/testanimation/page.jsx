3e59d8800e7b84af84db727ca2a77c5f27c23cfe
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-27 12:10:14 -0500

    Add framer motion page transitions

--- src/app/testanimation/page.jsx
+++ src/app/testanimation/page.jsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const TestPage = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+
 
   const variants = {
     variant1: {
@@ -29,7 +33,7 @@ const TestPage = () => {
   return (
     <div className='h-full flex items-center justify-center'>
       <motion.div 
-        className='w-96 h-96 bg-red-500 rounded' 
+        className={`w-96 h-96 ${darkMode ? 'bg-red-500' : 'bg-green-500'} rounded `}
         // initial={{ x:-100 }}
         variants={variants}
         animate='variant2'  // {condition ? 'variant2' : 'variant1'}
@@ -37,6 +41,18 @@ const TestPage = () => {
       >
         Yup
       </motion.div>
+
+      <div className='flex flex-col items-center justify-center'>
+        <div className={`w-64 h-64 flex items-center justify-center rounded-full border-solid border-[32px] ${darkMode ? 'bg-orange-500 border-black' : 'bg-black border-orange-500'}`}>
+          <div className={`w-32 h-32 flex items-center justify-center rounded-full border-solid border-[32px] ${darkMode ? 'bg-orange-500 border-black' : 'bg-black border-orange-500'}`}>
+          </div>
+        </div>
+        <button 
+          aria-pressed={darkMode}
+          className={`w-48 mt-8 flex items-center justify-center text-white ${darkMode ? 'bg-black': 'bg-orange-500'} rounded hover:bg-gray-700 hover:h-8 hover:w-52 active:translate-y-2 active:text-black transition-all duration-300`} onClick={(() => setDarkMode(!darkMode))}>
+          {darkMode ? 'Dark Mode' : 'Light Mode'}
+        </button>
+      </div>
     </div>
   )
 }
