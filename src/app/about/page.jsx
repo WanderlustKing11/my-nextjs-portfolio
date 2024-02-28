7493b167b1dd42ece331c32072f5313fefac56df
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-28 10:01:41 -0500

    Update About: add Skills

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -62,7 +62,97 @@ const AboutPage = () => {
 
             {/* SKILLS CONTAINER */}
             <div className='flex flex-col gap-12 justify-center'>
-              SKILLS
+              {/* SKILL TITLE */}
+              <h1 className='font-bold text-2xl'>SKILLS</h1>
+              {/* SKILL LIST */}
+              <div className='flex gap-4 flex-wrap'>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  JavaScript
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  TypeScript
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  React.js
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Next.js
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  SCSS
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Tailwind
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Bootstrap
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Material-UI
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Chakra-UI
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Framer Motion
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Canvas
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  MongoDB
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  SQL
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Node.js
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Express.js
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Webpack
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Babel
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Google Platform
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  AWS
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Docker
+                </div>
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Linux
+                </div>                
+                <div className='rounded p-2 text-sm cursor-pointer bg-black       text-white hover:bg-white hover:text-black'>
+                  Windows
+                </div>
+              </div>
+              {/* SCROLL SVG */}
+              <svg              
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                width={50}
+                height={50}
+              >
+                <path
+                  d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+                <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+                <path
+                  d="M15 11L12 14L9 11"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+              </svg>
             </div>
 
             {/* EXPERIENCE CONTAINER */}
